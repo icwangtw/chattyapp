27bb719af4a55a5ff5acd7ca68aaaab94f7a0d00
Stephen <stephenicwang@gmail.com>
2018-05-16 19:51:56 +0000

    able to Displaying Incoming Messages

--- src/App.jsx
+++ src/App.jsx
@@ -2,45 +2,34 @@ import React, {Component} from 'react';
 import ChatBar from "./ChatBar.jsx"
 import MessageList from "./MessageList.jsx"
 
-const rando = (alphabet => {
-  const alphabetLength = alphabet.length;
-  const randoIter = (key, n) => {
-    if (n === 0) {
-      return key;
-    }
-    const randoIndex = Math.floor(Math.random() * alphabetLength);
-    const randoLetter = alphabet[randoIndex];
-    return randoIter(key + randoLetter, n - 1);
-  };
-  return () => randoIter("", 10);
-})("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
-
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
       messages: [
-      { id: "1", username: "Bob", content: "Has anyone seen my marbles?", },
-      { id: "2", username: "Anonymous", content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."}
+      { id: 1, username: "Bob", content: "Has anyone seen my marbles?", },
+      { id: 2, username: "Anonymous", content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."}
       ]
     }
     this.onNewPost = this.onNewPost.bind(this);
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage)
-      this.setState({messages: messages})
-    }, 3000);
+    this.socket = new WebSocket("ws://localhost:3001")
+    console.log("connected to server")
+    this.socket.onmessage = (event) => {
+      const inboundmsg = JSON.parse(event.data)
+      const newmessages = this.state.messages.concat(inboundmsg)
+      this.setState({messages: newmessages})
+      console.log(inboundmsg);
+    }
   }
 
   onNewPost(post) {
-      const user = this.state.currentUser.name
-      const newMessage = {id: rando(), username: user, content: post};
-      const messages = this.state.messages.concat(newMessage)
-      this.setState({messages: messages})
+    const user = this.state.currentUser.name
+    const newMessage = {username: user, content: post};
+    this.socket.send(JSON.stringify(newMessage))
   }
 
   render() {
