ef1a6d216884e07ec9c9059e01fbeda10d50f2dc
Stephen <stephenicwang@gmail.com>
2018-05-18 00:13:03 +0000

    working css

--- regextest.js
+++
@@ -1,2 +0,0 @@
-const aaa = /(jpg$|png$|gif$)/.test("http://via.placeholder.com/350x150.gif")
-console.log(aaa)
--- src/Message.jsx
+++ src/Message.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 class Message extends React.Component {
   render () {
     const {id, username, content, type} = this.props.eachMessage;
-    const imageMatch = /(jpg$|png$|gif$)/;
+    const imageMatch = /^http.*(\.jpg$|\.png$|\.gif$)/i;
     return (
       <div>
         {type === "msg" ?
           <div className="message">
           <span className="message-username">{username}</span>
           {imageMatch.test(content) ?
-            <img src = {content}/>
+            <div className="message-content"><img src = {content}/></div>
             :
             <span className="message-content">{content}</span>
           }
